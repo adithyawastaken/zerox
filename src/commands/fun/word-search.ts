99bc9b7f4936a639fa83975e3f9e885d9aff7399
Adithya <adithyan2180@gmail.com>
2024-05-28 12:21:02 +0530

    bruvok

--- src/commands/fun/word-search.ts
+++ src/commands/fun/word-search.ts
@@ -24,8 +24,7 @@ export default {
 			const response = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`);
 			const data = await response.json();
 			const meaning = data[0].meanings[0].definitions[0].definition;
-			await interaction.editReply(`Word: ${word}
-			Meaning: ${meaning}`)
+			await interaction.editReply(`Word: ${word}\nMeaning: ${meaning}`)
 		}
 		catch {
 			await interaction.editReply(`Sorry, I couldn't find the meaning of '${word}'`)
